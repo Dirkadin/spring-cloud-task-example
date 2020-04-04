32301859ef8e9780346576f929ce88473f7ca2d8
Dirkadin <dirkadin@gmail.com>
2020-04-04 17:31:07 -0500

    Added gradle config

--- src/test/java/com/dirk/task/TaskApplicationTests.java
+++ src/test/java/com/dirk/task/TaskApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskApplicationTests {
+public class TaskApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
