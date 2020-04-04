f9920e148419d1ee817445ff47377080a09e6ca2
Dirkadin <dirkadin@gmail.com>
2020-04-04 16:52:07 -0500

    Initial commit

---
+++ src/main/java/com/dirk/task/TaskApplication.java
@@ -0,0 +1,37 @@
+package com.dirk.task;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.listener.TaskExecutionListener;
+import org.springframework.cloud.task.listener.annotation.BeforeTask;
+import org.springframework.cloud.task.repository.TaskExecution;
+
+@SpringBootApplication
+@EnableTask
+public class TaskApplication implements CommandLineRunner, TaskExecutionListener {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Welcome to " + args[0]);
+    }
+
+    @Override
+    public void onTaskStartup(TaskExecution taskExecution) {
+        System.out.println("Taskname " + taskExecution.getTaskName() + " " + taskExecution.getExecutionId() + " started....");
+    }
+
+    @Override
+    public void onTaskEnd(TaskExecution taskExecution) {
+        System.out.println("Taskname " + taskExecution.getTaskName() + " " + taskExecution.getExecutionId() + " completed....");
+    }
+
+    @Override
+    public void onTaskFailed(TaskExecution taskExecution, Throwable throwable) {
+        System.out.println("Taskname " + taskExecution.getTaskName() + " " + taskExecution.getExecutionId() + " failed....");
+    }
+}
---
+++ src/test/java/com/dirk/task/TaskApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dirk.task;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
